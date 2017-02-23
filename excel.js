c16c84a9ff5ea18fc548c8a0a8d5c766210e3d7c
cayres <rcayres18@gmail.com>
2017-02-22 22:17:52 -0300

    tabela

--- excel.js
+++ excel.js
@@ -1,16 +1,30 @@
 var Excel = React.createClass({
     displayName: 'Excel',
-    render: function(){
+
+    getInitialState: function () {
+        return { data: this.props.initialData }
+    },
+
+    render: function () {
         return (
             React.DOM.table(null,
                 React.DOM.thead(null,
                     React.DOM.tr(null,
-                        this.props.headers.map(function(title, idx){
-                            return React.DOM.th({key: idx}, title);
+                        this.props.headers.map(function (title, idx) {
+                            return React.DOM.th({ key: idx }, title);
                         })
                     )
-                )
+                ),
+                React.DOM.tbody(null,
+                this.state.data.map(function (row, idx) {
+                    return (React.DOM.tr({key: idx},
+                        row.map(function (cell, idx) {
+                            return React.DOM.td({ key: idx }, cell);
+                        }))
+                    )
+                }))
             )
+            
         );
     }
 });
